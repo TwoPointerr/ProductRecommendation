314f09bb2e61209266c27dea7ec9fc1ae9a51f9a
mandharet <tejasm@skiller.com>
2021-04-21 11:59:41 +0530

    some shop models created

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Cart, CartProduct, Category, Order, Product
 
+
+admin.site.register([Customer, Cart, CartProduct, Category, Order, Product])
 # Register your models here.
