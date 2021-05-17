6b81643665cac95ac1244f76a6c211579abd809e
PremMevada <premmevada@gmail.com>
2021-05-17 22:15:13 +0530

    color filter entries

--- eshop/settings.py
+++ eshop/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mathfilters',
     'apps.main',
     'apps.accounts',
     'apps.seller_accounts',
